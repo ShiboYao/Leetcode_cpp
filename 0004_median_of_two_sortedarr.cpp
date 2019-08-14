a7651505b0b27d12388483b3ae04b9d54c1330e5
Shibo Yao <espoyao@gmail.com>
2019-08-14 19:21:22 -0400

    update

--- 0004_median_of_two_sortedarr.cpp
+++ 0004_median_of_two_sortedarr.cpp
@@ -5,14 +5,45 @@ using namespace std;
 
 class Solution {
 public:
+   int findkth(vector<int>::iterator a, int n1, vector<int>::iterator b, int n2, int k){
+        if (n1 > n2)
+            return findkth(b, n2, a, n1, k);
+        if (n1 == 0)
+            return *(b+k-1);
+        if (k == 1)
+            return min(*a, *b);
+
+        int i = min(k/2, n1), j = k-i;
+        if (a[i-1] < b[j-1])
+            return findkth(a+i, n1-i, b, n2, k-i);
+        else if (a[i-1] > b[j-1])
+            return findkth(a, n1, b+j, n2-j, k-j);
+        else
+            return a[i-1];
+    }
+    
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
+        int n1 = nums1.size();
+        int n2 = nums2.size();
+        int total = n1+n2;
+        vector<int>::iterator a = nums1.begin();
+        vector<int>::iterator b = nums2.begin();
         
+        if (total%2 != 0)
+            return findkth(a, n1, b, n2, total/2+1);
+        else
+            return (findkth(a, n1, b, n2, total/2) + findkth(a, n1, b, n2, total/2+1))/2.0;
     }
 };
 
 
-int main(){
 
+int main(){
+    vector<int> a{1,2};
+    vector<int> b{3,4};
+    Solution s;
+    double r = s.findMedianSortedArrays(a, b);
+    cout << r << endl;
 
     return 0;
 }
