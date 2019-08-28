fc6946880320db02fceb55cbcea473c668ac80df
Shibo Yao <espoyao@gmail.com>
2019-08-28 12:21:52 -0400

    init

---
+++ 0111_minimum_depth_binary_tree.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+using namespace std;
+
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+
+class Solution {
+public:
+    int minDepth(TreeNode* root) {
+        if (!root)
+            return 0;
+        if (!root->left)
+            return 1 + minDepth(root->right);
+        if (!root->right)
+            return 1 + minDepth(root->left);
+
+        return 1 + min(minDepth(root->left), minDepth(root->right));
+    }
+};
+
+
+
+int main(){
+    TreeNode* r = new TreeNode(3);
+    r->left = new TreeNode(9);
+    r->right = new TreeNode(20);
+    r->right->left = new TreeNode(15);
+    r->right->right = new TreeNode(7);
+    Solution s;
+    cout << s.minDepth(r) << endl;
+
+    return 0;
+}
