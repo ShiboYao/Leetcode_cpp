daa2521624461c29704531d7b19509dd0cdc5906
Shibo Yao <espoyao@gmail.com>
2019-09-13 09:31:48 -0400

    iterative solution, better

--- 0785_is_graph_bipartite.cpp
+++ 0785_is_graph_bipartite.cpp
@@ -1,8 +1,40 @@
 #include <iostream>
 #include <vector>
+#include <queue>
 using namespace std;
 
 
+class Solution {
+public:
+    bool isBipartite(vector<vector<int>>& graph){
+        int n = graph.size();
+        vector<int> visited(n,0);
+        for (int i = 0; i < n; i++){
+            if (visited[i] == 0){
+                queue<int> q;
+                q.push(i);
+                visited[i] = 1;
+                while (!q.empty()){
+                    int node = q.front();
+                    q.pop();
+                    for (int neigh : graph[node]){
+                        if (!visited[neigh]){
+                            visited[neigh] = -visited[node];
+                            q.push(neigh);
+                        }
+                        else if (visited[neigh] == visited[node])
+                            return false;
+                    }
+                }
+            }
+        }
+
+        return true;
+    }
+};
+
+
+/*
 class Solution {
 public:
     bool util(vector<int>& visited, vector<vector<int>> graph, int node, int t){
@@ -30,7 +62,7 @@ public:
         return true;
     }
 };
-
+*/
 
 
 int main(){
