528aa44cafda42ceb9ef5b86e3380e2d3ea44990
Shibo Yao <espoyao@gmail.com>
2019-09-16 16:55:20 -0400

    init, efficient but need more concise sol.

--- 0297_serialize_deserialize_binary_tree.cpp
+++ 0297_serialize_deserialize_binary_tree.cpp
@@ -16,17 +16,20 @@ class Codec {
 public:
     // Encodes a tree to a single string.
     string serialize(TreeNode* root) {
+        if (!root)
+            return "[]";
         string r = "[";
         TreeNode* p;
         vector<TreeNode*> stack{root};
         vector<TreeNode*> t;
-        while (!stack.empty()){
-            
+        int count = 1;
+        while (count){
+            count = 0;
             while (!stack.empty()){
                 p = stack.back();
                 stack.pop_back();
                 if (p){
-                    r += p->val;
+                    r += to_string(p->val);
                     t.push_back(p->left);
                     t.push_back(p->right);
                 }
@@ -35,6 +38,7 @@ public:
                 r += ',';
             }
             while (!t.empty()){
+                if (t.back()) count++;
                 stack.push_back(t.back());
                 t.pop_back();
             }
@@ -49,37 +53,36 @@ public:
         vector<TreeNode*> parent, t;
         TreeNode* r;
         int i = 1, j = 1, n = data.size()-1;
+        if (j == n)
+            return NULL;
         while (i < n && data[i] != ',')
             i++;
-        if (data[j] == 'n')
-            return NULL;
-        else {
-            r = new TreeNode(stoi(data.substr(j,i)));
-            parent.push_back(r);
-            j = ++i;
-        }
+        r = new TreeNode(stoi(data.substr(j,i-j)));
+        parent.push_back(r);
+        j = ++i;
+        
         TreeNode* p;
         while (!parent.empty()){
-            p = parent.back();
-            parent.pop_back();
-            while (i < n && data[i] != ',')
-                i++;
-            if (data[j] == 'n')
-                p->left = NULL;
-            else {
-                p->left = new TreeNode(stoi(data.substr(j,i)));
-                t.push_back(p->left);
+            while (!parent.empty()){
+                p = parent.back();
+                parent.pop_back();
+                while (i < n && data[i] != ',')
+                    i++;
+                if (j == i) break;
+                if (data[j] != 'n'){
+                    p->left = new TreeNode(stoi(data.substr(j,i-j)));
+                    t.push_back(p->left);
+                }
+                j = ++i;
+                while (i < n && data[i] != ',')
+                    i++;
+                if (j == i) break;
+                if (data[j] != 'n'){
+                    p->right = new TreeNode(stoi(data.substr(j,i-j)));
+                    t.push_back(p->right);
+                }
+                j = ++i;
             }
-            j = ++i;
-            while (i < n && data[i] != ',')
-                i++;
-            if (data[j] == 'n')
-                p->right = NULL;
-            else {
-                p->right = new TreeNode(stoi(data.substr(j,i)));
-                t.push_back(p->right);
-            }
-            j = ++i;
             while (!t.empty()){
                 parent.push_back(t.back());
                 t.pop_back();
@@ -95,15 +98,14 @@ public:
 // codec.deserialize(codec.serialize(root));
 
 
-
 int main(){
-    TreeNode* p = new TreeNode(1);
-    p->left = new TreeNode(2);
-    p->right = new TreeNode(3);
-    p->right->left = new TreeNode(4);
-    p->right->right = new TreeNode(5);
+    //string a = "[1,2,3,1,3,2,4]"; 
+    //string a = "[1,2,3,null,null,4,5]";
+    string a = "[]";
+    //string a = "[1]";
     Codec s;
-    string str = s.serialize(p);
+    TreeNode* r = s.deserialize(a);
+    string str = s.serialize(r);
     cout << str << endl;
 
     return 0;
