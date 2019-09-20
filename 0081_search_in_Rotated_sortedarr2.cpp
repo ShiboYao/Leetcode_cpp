ee2f1e07855029bd8d1f6304149f35c80972524c
Shibo Yao <espoyao@gmail.com>
2019-09-20 11:26:30 -0400

    update

--- 0081_search_in_Rotated_sortedarr2.cpp
+++ 0081_search_in_Rotated_sortedarr2.cpp
@@ -2,7 +2,7 @@
 #include <vector>
 using namespace std;
 
-
+/*
 class Solution {
 public:
     bool search(vector<int>& nums, int target) {
@@ -33,6 +33,43 @@ public:
         return false;
     }   
 };
+*/
+
+
+class Solution {
+public:
+    bool search(vector<int>& nums, int target) {
+        int low = 0, high = nums.size()-1, mid;
+        while (low <= high){
+            while (low < high && nums[low] == nums[low+1]) low++;
+            while (low < high && nums[high] == nums[high-1]) high--;
+            mid = (low+high)/2;
+            if (nums[mid] == target)
+                return true;
+            if (nums[mid] > nums[high]){
+                if (nums[mid] > target && nums[low] <= target)
+                    high = mid-1;
+                else
+                    low = mid+1;
+            }
+            else if (nums[mid] < nums[low]){
+                if (nums[mid] < target && nums[high] >= target)
+                    low = mid+1;
+                else
+                    high = mid-1;
+            }
+            else {
+                if (nums[mid] > target)
+                    high = mid-1;
+                else
+                    low = mid+1;
+            }
+        }
+
+        return false;
+    }
+};
+
 
 
 int main(){
