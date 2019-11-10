5a7ff24c46d1cbbd4e736c482a54bca094303954
ShiboYao <espoyao@gmail.com>
2019-11-10 18:33:21 -0500

    init

---
+++ 0746_min_cost_climbing_stairs.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+
+class Solution {
+public:
+    int minCostClimbingStairs(vector<int>& cost) {
+        int n = cost.size();
+        for (int i = 2; i < n; i++)
+            cost[i] += min(cost[i-1],cost[i-2]);
+
+        return min(cost[n-1],cost[n-2]);
+    }
+};
+
+
+
+int main(){
+    vector<int> a{10,15,20};
+    Solution s;
+    cout << s.minCostClimbingStairs(a) << endl;
+
+    return 0;
+}
